49de6561252adae61dd0818cb64d569cc0ce8113
Vaibhav <vaibhavagarwal3928@gmail.com>
2023-01-07 20:16:23 +0530

    upate and delete forms

---
+++ src/main/java/com/wazooinc/springboottodoapplication/config/todoItemRepository.java
@@ -0,0 +1,5 @@
+package com.wazooinc.springboottodoapplication.config;
+
+public class todoItemRepository {
+
+}
--- src/main/java/com/wazooinc/springboottodoapplication/controllers/TodoItemController.java
+++ src/main/java/com/wazooinc/springboottodoapplication/controllers/TodoItemController.java
@@ -1,20 +1,46 @@
 package com.wazooinc.springboottodoapplication.controllers;
 
+import java.time.Instant;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.wazooinc.springboottodoapplication.models.TodoItem;
+import com.wazooinc.springboottodoapplication.repositories.TodoItemRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
     
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+    
     @GetMapping("/")
     public ModelAndView index() {
-        logger.debug("request to GET index");
+        logger.info("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
-    
-}
+    }
+
+    @PostMapping("/todo/{id}")
+    public String updateTodoItem(@PathVariable("id") long id, @Valid TodoItem todoItem, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
 }
